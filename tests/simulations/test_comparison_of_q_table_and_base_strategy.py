342b3ec7909740c688ae6734f9fbaad172fd2f64
MatveyFilippov <matdufi2004@gmail.com>
2025-05-16 07:02:54 +0300

    bug fix with paths

--- tests/simulations/test_comparison_of_q_table_and_base_strategy.py
+++ tests/simulations/test_comparison_of_q_table_and_base_strategy.py
@@ -3,14 +3,16 @@ from environment.default_game import DefaultGame
 from learning_engine.q_learning import QTable
 from tests.simulations.game_simulator import GameSimulator
 import time
+import os
 
 
 if __name__ == "__main__":
     sim_by_basic_strategy = GameSimulator(
-        game_environment=DefaultGame(4), agent=AgentForDefaultGameByBasicStrategy()
+        game_environment=DefaultGame(4), agent=AgentForDefaultGameByBasicStrategy(),
     )
     sim_by_q_table = GameSimulator(
-        game_environment=DefaultGame(4), agent=AgentForDefaultGameByQTable(QTable.load("../../q_table.tbjh"))
+        game_environment=DefaultGame(4),
+        agent=AgentForDefaultGameByQTable(QTable.load(os.path.join("..", "..", "q_table.tbjh"))),
     )
     sim_by_basic_strategy.start()
     sim_by_q_table.start()
--- tests/simulations/test_q_table_in_simulator.py
+++ tests/simulations/test_q_table_in_simulator.py
@@ -3,11 +3,13 @@ from environment.default_game import DefaultGame
 from learning_engine.q_learning import QTable
 from tests.simulations.game_simulator import GameSimulator
 import time
+import os
 
 
 if __name__ == "__main__":
     with GameSimulator(
-            game_environment=DefaultGame(4), agent=AgentForDefaultGameByQTable(QTable.load("../../q_table.tbjh"))
+            game_environment=DefaultGame(4),
+            agent=AgentForDefaultGameByQTable(QTable.load(os.path.join("..", "..", "q_table.tbjh"))),
     ) as sim:
         while sim.is_running:
             sim_info = f"Score: {sim.score}"
